392093d1b4919590593bd980504b35952ae375fb
Rafel460 <rafaelmonte46@outlook.com>
2020-06-07 20:35:54 -0300

    v com MarketPlace

--- public/market-place/market-place.php
+++ public/market-place/market-place.php
@@ -12,22 +12,29 @@
     $pagina = "market-place";
     require_once("../tela-inicio/select-user.php");
     require_once("../coisas_bootstrap/navbar-user.php");
+    require_once("select-products.php");
     ?>
     <br>
+    <!-- TEM QUE FAZER A CONSULTA ASSOCIADA COM O USUARIO QUE CRIOU O PRODUTO  -->
      <div class="row justify-content-md-center">
-        <? for($i = 0; $i < 3; $i++){ ?>
+        <? while($registros = mysqli_fetch_assoc($query)){ ?>
     
         <div class="card" style="width: 18rem;">
-        <img src="..." class="card-img-top" alt="...">
+        <img src=<?echo '"'.$registros['path'].'"'?> class="card-img-top image-responsive" alt="..." width="300px" height="300px">
         <div class="card-body">
-        <p class="card-text">Item <?=$i+1?></p>
+        <h5 class="card-title"><?=$registros['nomeprod']?></h5>
+        <p class="card-text"><?=$registros['descricao']?></p>
+        <p class="card-text text-right">Preço R$ <?=$registros['preco']?></p>
         </div>
         </div>
-       
+        &nbsp;&nbsp;&nbsp;
         
         <? }?>
+        
         </div>
-
-
+            <br><br>
+            <div class="row justify-content-md-center">
+            <a class="btn btn-outline-dark" href="inserir-produto.php">Colocar um Produto!</a>
+            </div>
 </body>
 </html>
